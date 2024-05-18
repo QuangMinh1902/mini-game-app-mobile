a82a6942ad40941031bc165fbffe6259a7610a64
Quang Minh PHAM <quangminh1575@gmail.com>
2024-05-18 11:54:34 +0200

    fea: improve button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
 export default function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Press!");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "orange" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: "#9935b7",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,16 +4,22 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton> Reset</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType={"numeric"}
+        autoCapitalize={"none"}
+        autoCorrect={"false"}
+      />
       <PrimaryButton> Confirm</PrimaryButton>
+      <PrimaryButton> Reset</PrimaryButton>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    backgroundColor: "#72062c",
+    backgroundColor: "#ba0d4c",
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
@@ -22,6 +28,18 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 2, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 1,
-    shadowColor:'red'
+    shadowColor: "red",
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
   },
 });
