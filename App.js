85ccd88453a50011a3f745305b3c33608ab52c6d
Quang Minh PHAM <quangminh1575@gmail.com>
2024-05-22 17:45:40 +0200

    fea: switching screens

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient colors={["#ddb52f", "#4e0b29"]} style={styles.rootScreen}>
       <ImageBackground
@@ -12,7 +25,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
@@ -23,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  backgroundImage : {
-    opacity:0.15
-  }
+  backgroundImage: {
+    opacity: 0.15,
+  },
 });
